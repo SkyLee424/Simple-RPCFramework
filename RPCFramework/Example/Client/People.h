33bbfdb73b09dcbd3d12a5f9a570a879ad5577a5
Sky_Lee <1350650238@qq.com>
2023-08-27 11:16:14 +0800

    People 类继承 Serializable 类，序列化更方便

--- RPCFramework/Example/Client/People.h
+++ RPCFramework/Example/Client/People.h
@@ -2,8 +2,9 @@
 
 #include <iostream>
 #include <string>
+#include "Serializable.hpp"
 
-class People
+class People : public Serializable
 {
 public:
     std::string name;
@@ -12,45 +13,24 @@ public:
 
     People() = default;
 
-    friend std::istream &operator>>(std::istream &is, People &people)
+    static std::istream &DeSerialize(std::istream &is, People &people)
     {
-        // 自定义的对象类型必须正确读入字符串对象（主要是空格问题)
-        // 这里规定所有的字符串对象都不包含 ' 字符
-        std::string temp;
-        auto parseTo = [&temp](std::string &target) -> int
-        {
-            if(temp.find('\'') != std::string::npos)
-            {
-                size_t start = temp.find('\'');
-                size_t end = temp.find('\'', start + 1);
-                if(end == std::string::npos)
-                    return -1;
-                target = std::move(temp.substr(start + 1, end - start - 1));
-            }
-            else target = std::move(temp);
-            return 0;
-        };
-
-        std::getline(is, temp);
-        if(parseTo(people.name) == -1)
-            throw std::runtime_error("姓名不合法！输入的姓名为：" + temp);
-        
-        is >> people.age;
-        if(!is)
-            throw std::runtime_error("输入年龄不合法！");
-        is.seekg(1, std::ios::cur);
-
-        std::getline(is, temp);
-        if(parseTo(people.BinZhou) == -1)
-            throw std::runtime_error("宾周不合法！输入的宾州为：" + temp);
-
+        Serializable::DeSerialize(is, people.name);
+        Serializable::DeSerialize(is, people.age);
+        Serializable::DeSerialize(is, people.BinZhou);
         return is;
     }
 
-    // 重载的 << 运算符的输出规则必须与 >> 运算符的输入规则一致，否则序列化错误
-    friend std::ostream &operator<<(std::ostream &os, const People &people)
+    static std::ostream &Serialize(std::ostream &os, const People &people)
     {
-        os << people.name << "\n" << people.age << "\n" << people.BinZhou;
+        Serializable::Serialize(os, people.name);
+        Serializable::Serialize(os, people.age);
+        Serializable::Serialize(os, people.BinZhou);
         return os;
+    }
+
+    bool operator<(const People &p) const
+    {
+        return this->age < p.age;
     }
 };
