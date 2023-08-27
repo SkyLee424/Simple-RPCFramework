1b907dbea55d6fb5fa6dd916a6cd2a354a7f54b5
Sky_Lee <1350650238@qq.com>
2023-08-27 11:15:24 +0800

    继承了 Serializable 类，序列化与反序列化更方便了

--- RPCFramework/includes/ProcedurePacket.hpp
+++ RPCFramework/includes/ProcedurePacket.hpp
@@ -4,61 +4,8 @@
 #include <memory>
 #include <tuple>
 #include <utility>
+#include "Serializable.hpp"
 
-template <typename ...Args>
-struct ProcedurePacket
-{
-    std::string name; // 过程的名称
-    std::tuple<Args ...> t; // tuple 用于保存传递的参数
-
-    ProcedurePacket() = default; // 缺陷：Args 不能包含引用类型的参数
-
-    ProcedurePacket(const std::string &name, Args... args)
-        :name(name), t(std::make_tuple(args...)) {}
-
-    template <typename ...X>
-    friend std::ostream& operator<<(std::ostream &os, const ProcedurePacket<X ...> &packet);
-
-    template <typename ...X>
-    friend std::istream& operator>>(std::istream &is, ProcedurePacket<X ...> &packet);
-};
-
-// std::string 对象在序列化时特殊处理
-template <typename T>
-typename std::enable_if<std::is_same<T, std::string>::value, void>::type
-expand_tuple_helper(std::ostream &os, const T &val)
-{
-    os << val.size() << " " << val << " ";
-}
-
-template <typename T>
-typename std::enable_if<!std::is_same<T, std::string>::value, void>::type
-expand_tuple_helper(std::ostream &os, const T &val)
-{
-    os << val << " ";
-}
-
-// std::string 对象在反序列化时特殊处理
-template <typename T>
-typename std::enable_if<std::is_same<T, std::string>::value, void>::type
-expand_tuple_helper(std::istream &is, T &val)
-{
-    size_t len;
-    is >> len;
-    is.seekg(1, std::ios::cur); // 跳过空格
-    char *buffer = new char[len + 1];
-    is.read(buffer, len);
-    buffer[len] = '\0';
-    val = buffer;
-    delete []buffer;
-}
-
-template <typename T>
-typename std::enable_if<!std::is_same<T, std::string>::value, void>::type
-expand_tuple_helper(std::istream &is, T &val)
-{
-    is >> val;
-}
 
 // 递归终点
 // 使用 std::enable_if 来判断是否将该模版函数加入到重载函数集中
@@ -85,8 +32,7 @@ template <size_t Index = 0, typename ...X>
 typename std::enable_if<Index < sizeof...(X), void>::type
 expand_tuple(std::ostream& os, const std::tuple<X...>& t)
 {
-    // os << std::get<Index>(t) << " ";
-    expand_tuple_helper(os, std::get<Index>(t));
+    Serializable::Serialize(os, std::get<Index>(t));
     expand_tuple<Index + 1, X...>(os, t);
 }
 
@@ -95,25 +41,34 @@ template <size_t Index = 0, typename ...X>
 typename std::enable_if<Index < sizeof...(X), void>::type
 expand_tuple(std::istream& is, std::tuple<X...>&t)
 {
-    // is >> std::get<Index>(t);
-    expand_tuple_helper(is, std::get<Index>(t));
+    Serializable::DeSerialize(is, std::get<Index>(t));
     expand_tuple<Index + 1, X...>(is, t);
 }
 
-// 重载的 << 运算符可以被用于序列化一个 ProcedurePacket 对象
-template <typename ...X>
-std::ostream& operator<<(std::ostream &os, const ProcedurePacket<X ...> &packet)
+template <typename ...Args>
+struct ProcedurePacket : public Serializable
 {
-    os << packet.name << " ";
-    expand_tuple(os, packet.t);
-    return os;
-}
+    std::string name; // 过程的名称
+    std::tuple<Args ...> t; // tuple 用于保存传递的参数
 
-// 重载的 >> 运算符可以被用于反序列化一个 ProcedurePacket 对象
-template <typename ...X>
-std::istream& operator>>(std::istream &is, ProcedurePacket<X ...> &packet)
-{
-    is >> packet.name;
-    expand_tuple(is, packet.t);
-    return is;
-}
+    ProcedurePacket() = default; // 缺陷：Args 不能包含引用类型的参数
+
+    ProcedurePacket(const std::string &name, Args... args)
+        :name(name), t(std::make_tuple(args...)) {}
+
+    template <typename ...X>
+    static std::ostream& Serialize(std::ostream &os, const ProcedurePacket<X ...> &packet)
+    {
+        os << packet.name << " ";
+        expand_tuple(os, packet.t);
+        return os;
+    }
+
+    template <typename ...X>
+    static std::istream& DeSerialize(std::istream &is, ProcedurePacket<X ...> &packet)
+    {
+        is >> packet.name;
+        expand_tuple(is, packet.t);
+        return is;
+    }
+};
--- RPCFramework/includes/ReturnPacket.hpp
+++ RPCFramework/includes/ReturnPacket.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Serializer.hpp"
+#include "Serializable.hpp"
 
 
 // 存储返回值类型的结构体（主要是用于实现支持返回值为 void 的函数）
@@ -17,7 +18,7 @@ struct RetType<void>
 };
 
 template <typename R>
-class ReturnPacket
+class ReturnPacket : public Serializable
 {
 public:
     using code_t = short;
@@ -28,10 +29,29 @@ public:
     static constexpr code_t NO_SUCH_PROCEDURE = 2;
 
     template <typename X>
-    friend std::ostream& operator<<(std::ostream &os, const ReturnPacket<X> &retPack);
+    static std::ostream& Serialize(std::ostream &os, const ReturnPacket<X> &retPack)
+    {
+        std::string temp = Serializer::Serialize(retPack.ret);
+        os << retPack.code << " " << temp.size() << " " << temp;
+        return os;
+    }
 
     template <typename X>
-    friend std::istream& operator>>(std::istream &is, ReturnPacket<X> &retPack);
+    static std::istream& DeSerialize(std::istream &is, ReturnPacket<X> &retPack)
+    {
+        size_t len;
+        std::string temp;
+        is >> retPack.code >> len;
+        is.seekg(1, std::ios::cur);
+
+        char *buffer = new char[len + 1];
+        is.read(buffer, len);
+        buffer[len] = '\0';
+        temp = buffer;
+
+        retPack.ret = Serializer::Deserialize<typename RetType<R>::type>(temp);
+        return is;
+    }
 
     ReturnPacket()
         :code(UNKNOWN) {}
@@ -70,29 +90,3 @@ private:
     code_t code;
     ret_t ret;
 };
-
-// 还要实现 << 和 >> 
-template <typename R>
-std::ostream& operator<<(std::ostream &os, const ReturnPacket<R> &retPack)
-{
-    std::string temp = Serializer::Serialize(retPack.ret);
-    os << retPack.code << " " << temp.size() << " " << temp;
-    return os;
-}
-
-template <typename R>
-std::istream& operator>>(std::istream &is, ReturnPacket<R> &retPack)
-{
-    size_t len;
-    std::string temp;
-    is >> retPack.code >> len;
-    is.seekg(1, std::ios::cur);
-
-    char *buffer = new char[len + 1];
-    is.read(buffer, len);
-    buffer[len] = '\0';
-    temp = buffer;
-
-    retPack.ret = Serializer::Deserialize<typename RetType<R>::type>(temp);
-    return is;
-}
