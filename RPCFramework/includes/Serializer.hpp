3f24d890b008a17529061b554b3061a3cdb32863
Sky_Lee <1350650238@qq.com>
2023-08-27 11:13:37 +0800

    添加了 Serializable 类，可以支持内置类型和常用 STL 的序列化与反序列化

--- RPCFramework/includes/Serializer.hpp
+++ RPCFramework/includes/Serializer.hpp
@@ -3,45 +3,30 @@
 #include <sstream>
 #include <string>
 #include <typeinfo>
+#include "Serializable.hpp"
 
 /**
  * @brief 简单的序列化与反序列化工具，可以实现任意对象的序列化和反序列化过程，但该对象必须重载 << 和 >> 运算符
- * 
+ *
  */
-class Serializer {
+class Serializer
+{
 public:
     template <typename T>
-    static std::string Serialize(const T& object) {
+    static std::string Serialize(T &object)
+    {
         std::ostringstream os;
-        os << object;
+        Serializable::Serialize(os, object);
         return os.str();
     }
 
     template <typename T>
-    static typename
-    std::enable_if<!std::is_same<T, std::string>::value, T>::type
-    Deserialize(const std::string& serializedData) {
+    static T
+    Deserialize(const std::string &serializedData)
+    {
         std::istringstream is(serializedData);
         T object;
-        is >> object;
-        return object;
-    }
-
-    //  对于反序列化对象为 string 的，特殊处理
-    template <typename T>
-    static typename
-    std::enable_if<std::is_same<T, std::string>::value, std::string>::type
-    Deserialize(const std::string& serializedData) {
-        std::istringstream is(serializedData);
-        T object;
-        std::string temp;
-        while (std::getline(is, temp))
-        {
-            object += temp;
-            if(!is.eof())
-                object += '\n';
-        }
-        
+        Serializable::DeSerialize(is, object);
         return object;
     }
 };
